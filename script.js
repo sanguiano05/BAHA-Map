6ed28ab9c8cc0365550b1e7636a373febd5f3bfc
sanguiano05 <sanguiano05@berkeley.edu>
2025-10-27 10:07:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FuZ3VpYW5vMDUiLCJhIjoiY21oOWM0cWd5MGx5dzJqcHVleGw1eHR1NiJ9.g_IYdcIdJK3m0aVFm_8F8Q';
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+   style: 'mapbox://styles/sanguiano05/cmh9cf3i800pt01sr2ypvdh5t',
+  center: [-122.253, 37.872], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 14 // starting zoom
+    });
+
+    
